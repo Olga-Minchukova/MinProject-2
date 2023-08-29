7547b297fb45fdc04571be83816631f5e2b4f2d9
OLGA Minchukova <omin1110@gmail.com>
2023-08-29 19:54:28 +0300

    .gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tickedPrice = 10_000;
+        int priceForOneMiles = 20;
+        int miles = tickedPrice / priceForOneMiles;
+        System.out.println(" Вам начислено "+miles +" бонусых миль");
+    }
+}
